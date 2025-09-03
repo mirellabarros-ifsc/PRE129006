0fc333c2a5c5b8788b9af302d490287ff65c9a23
Mirella Barros <mirella.b@aluno.ifsc.edu.br>
2025-09-03 08:33:49 -0300

    Atualização com os valores teóricos

--- va_mista.py
+++ va_mista.py
@@ -17,25 +17,37 @@ X[idx2] = np.random.binomial(1, 2/3, idx2.sum())
 
 dx = 0.01
 xs = np.arange(-.5, 2.5, dx)
-# pdf_X_teo = ...
+pdf_X_teo = 1/8 * (0 <= xs) * (xs < 2) # apenas a parte contínua
 pdf_X_sim = pre.hist(X, xs)
-# cdf_X_teo = ...
+cdf_X_teo = (
+    (0)                 * (xs < 0) +
+    (1/4 + xs/8)        * (0 <= xs) * (xs < 1) +
+    (7/8 + (xs - 1)/8)  * (1 <= xs) * (xs < 2) +
+    (1)                 * (2 <= xs)
+)
 cdf_X_sim = np.cumsum(pdf_X_sim) * dx
-# ev_X_teo = ... (media ou expected value)
+ev_X_teo = 3/4
 ev_X_sim = np.mean(X)
 
 # Saída
 plt.subplot(2,1,1)
-plt.bar(xs, pdf_X_sim, width=0.8*dx)
+plt.bar(xs, pdf_X_sim, width=1*dx, color="y")
+plt.plot(xs, pdf_X_teo, linewidth=3, color="b")
+plt.annotate("", xytext=(0, 0), xy=(0,1/4), arrowprops=pre.arrowprops)
+plt.annotate("", xytext=(1, 0), xy=(1,1/2), arrowprops=pre.arrowprops)
+plt.ylim(-0.2, 2.2)
 plt.ylim(-0.1, 0.6)
 plt.xlabel("$x")
 plt.ylabel("$f_X(x)$")
 plt.grid()
 plt.subplot(2,1,2)
-plt.plot(xs, cdf_X_sim)
+plt.plot(xs, cdf_X_sim, linewidth=4, color="y")
+plt.plot(xs, cdf_X_teo, linewidth=2, color="b", linestyle=("--"))
 plt.ylim(-0.2, 1.2)
 plt.xlabel("$x")
 plt.ylabel("$F_X(x)$")
 plt.grid()
 print(f"Simulado: E[X]: {ev_X_sim:g}")
 plt.show()
+
+print(f"Valor teórico: {ev_X_teo}")
